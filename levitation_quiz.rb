3f8eaa384955b6399317b38807b0666ae0b4144e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-21 17:53:39 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,13 @@
 
 def levitation_quiz
 	#your code here
-end
+  loop do
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+  end
 
+  puts "You passed the quiz!"
+  
+end
 
